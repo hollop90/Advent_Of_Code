4413f4a2e4d1c2f68bfe6e4eb23d32f9a7f6f45a
hollop90 <nozolo96@gmail.com>
2022-09-04 00:29:10 +0100

    Updated final step to use bitsets This makes converting to intiger values easier

--- day3/main.cpp
+++ day3/main.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <string>
 #include <array>
+#include <bitset>
+#include <chrono>
 
 constexpr int NUM_BITS = 12;
-
 std::array<int, NUM_BITS> count = {};
 
 int main()
@@ -33,45 +34,28 @@ int main()
         }
     }
 
-    std::cout << "GAMMA: ";
-    uint16_t gamma{};
-    // int tracker = 5;
+    std::bitset<NUM_BITS> gamma;
+    int tracker = count.size();
 
-    // for (int i = 0; i < count.size(); i++)
-    // {
-    //     if (count[i] > 0)
-    //     {
-    //         gamma |= 1UL << (tracker - 1);
-    //     }
-    //     else
-    //     {
-    //         gamma &= ~(1UL << (tracker - 1));
-    //     }
-    //     tracker--;
-    // }
-    // std::cout << gamma;
-    for(auto i: count){
-
-        if(i > 0) {
-            std::cout << 1;
-        }else{
-            std::cout << 0;
-        }
-
-    }
-    std::cout << std::endl;
-
-    std::cout << "EPSILON: ";
-    for (auto i : count)
+    for (int i = 0; i < count.size(); i++)
     {
-        if (i > 0)
+        if (count[i] > 0)
         {
-            std::cout << 0;
+            gamma.set(tracker -1);
         }
         else
         {
-            std::cout << 1;
+            gamma.set(tracker - 1, 0);
         }
+        tracker--;
     }
-    std::cout << std::endl;
+
+    std::cout << "GAMMA: " << gamma.to_ulong() << std::endl;
+
+    std::bitset<NUM_BITS> epsilon(gamma);
+    epsilon.flip();
+
+    std::cout << "EPSILON: " << epsilon.to_ulong() << std::endl;
+
+    std::cout << "POWER: " << (gamma.to_ulong() * epsilon.to_ulong()) << std::endl;
 }
