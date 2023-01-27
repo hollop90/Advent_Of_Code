9a010579b9037c128f8c76980b7ea7b9f8c77a8a
hollop90 <nozolo96@gmail.com>
2023-01-27 17:16:08 +0000

    feat(parseLine): Function now returns vaulue rather than cause side effects
    Currently the parseLine Function mutates a couple of std::pair objects
    in the global scope. This change makes the function return these objects
    instead.

--- day5/main.cpp
+++ day5/main.cpp
@@ -5,43 +5,59 @@
 #include <tuple>
 #include <array>
 #include <exception>
+#include <vector>
 
 template <typename T>
-void printPair(std::pair<T, T> aPair)
+void printPair(std::pair<T, T> _pair)
 {
-    std::cout << "[" << aPair.first << ", " << aPair.second << "]" << '\n';
+    std::cout << "[" << _pair.first << ", " << _pair.second << "]" << '\n';
 }
 
-template <typename T>
-void parseLine(std::string a_line, std::pair<T,T> &a_pair, std::pair<T,T> &b_pair)
+std::pair<std::pair<int, int>, std::pair<int, int>> 
+parseLine ( std::string _line)
 {
     // Example of input line: "0,9 -> 5,9"
-    std::size_t     posTrack    = 0;
-    std::string     delim       = "->";
+    std::pair<int, int> begin;
+    std::pair<int, int> end;
+    std::size_t posTrack = 0;
+    std::string delim = "->";
+    std::string tmp_string;
 
-    a_pair.first = std::stoi(a_line, &posTrack);
-    a_line = a_line.substr(posTrack + 1);
-    a_pair.second = std::stoi(a_line, &posTrack);
-    a_line = a_line.substr(a_line.find(delim) + delim.length());
-    
-    b_pair.first = std::stoi(a_line, &posTrack);
-    a_line = a_line.substr(posTrack + 1);
-    b_pair.second = std::stoi(a_line, &posTrack);
+    tmp_string = _line.substr(0, _line.find(delim));
+    begin.first = std::stoi(tmp_string, &posTrack);
+    tmp_string = tmp_string.substr(posTrack + 1);
+    begin.second = std::stoi(tmp_string);
+
+    tmp_string = _line.substr(_line.find(delim) + delim.length());
+    end.first = std::stoi(tmp_string, &posTrack);
+    tmp_string = tmp_string.substr(posTrack + 1);
+    end.second = std::stoi(tmp_string);
+
+    return std::make_pair(begin, end);
 }
 
 int main()
 {
     std::ifstream inFile("input.txt");
-
     std::string inString;
-    std::getline(inFile, inString);
 
-    std::pair<int, int> lineBegin;
-    std::pair<int, int> lineEnd;
+    std::vector< std::pair< std::pair<int, int>, 
+                            std::pair<int, int> > > myLines;
 
-    auto line = std::make_pair(lineBegin, lineEnd);
-
-    parseLine(inString, lineBegin, lineEnd);
-    printPair(lineBegin);
-    printPair(lineEnd);
+    while (!inFile.eof())
+    {
+        std::getline(inFile, inString);
+        if (!inString.compare(""))
+            break;
+        myLines.push_back(parseLine(inString));
+    }
+    
+    for (auto line : myLines)
+    {
+        std::cout << "Begin:\t";
+        printPair(line.first);
+        std::cout << "End:\t";
+        printPair(line.second);
+        std::cout << "\n";
+    }
 }
