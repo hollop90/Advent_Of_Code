05c302dd4d53d910227082c73da8fb272cc2b1d8
hollop90 <nozolo96@gmail.com>
2023-01-27 00:33:12 +0000

    feat: Add data parsing and printing functions
    Adds the function parseLine() to load the line segments from the file
    and the function printPait() to print the line end points

--- day5/main.cpp
+++ day5/main.cpp
@@ -6,34 +6,42 @@
 #include <array>
 #include <exception>
 
+template <typename T>
+void printPair(std::pair<T, T> aPair)
+{
+    std::cout << "[" << aPair.first << ", " << aPair.second << "]" << '\n';
+}
+
+template <typename T>
+void parseLine(std::string a_line, std::pair<T,T> &a_pair, std::pair<T,T> &b_pair)
+{
+    // Example of input line: "0,9 -> 5,9"
+    std::size_t     posTrack    = 0;
+    std::string     delim       = "->";
+
+    a_pair.first = std::stoi(a_line, &posTrack);
+    a_line = a_line.substr(posTrack + 1);
+    a_pair.second = std::stoi(a_line, &posTrack);
+    a_line = a_line.substr(a_line.find(delim) + delim.length());
+    
+    b_pair.first = std::stoi(a_line, &posTrack);
+    a_line = a_line.substr(posTrack + 1);
+    b_pair.second = std::stoi(a_line, &posTrack);
+}
+
 int main()
 {
     std::ifstream inFile("input.txt");
 
     std::string inString;
     std::getline(inFile, inString);
-    // std::cout << inString << std::endl;
 
     std::pair<int, int> lineBegin;
     std::pair<int, int> lineEnd;
 
-    // split string in two
-    std::size_t trackPos = 0;
-    std::string tmpStr = inString;
-    lineBegin.first = std::stoi(tmpStr, &trackPos);
-    tmpStr = tmpStr.substr(trackPos + 1);
-    lineBegin.second = std::stoi(tmpStr, &trackPos);
-    tmpStr = tmpStr.substr(tmpStr.find("->") + 2);
+    auto line = std::make_pair(lineBegin, lineEnd);
 
-    std::cout << lineBegin.first << " " << lineBegin.second << std::endl;
-
-    lineEnd.first = std::stoi(tmpStr, &trackPos);
-    tmpStr = tmpStr.substr(trackPos + 1);
-    lineEnd.second = std::stoi(tmpStr, &trackPos);
-
-    std::cout << lineEnd.first << " " << lineEnd.second << std::endl;
-
-    // get first pair
-
-    // get second pair
+    parseLine(inString, lineBegin, lineEnd);
+    printPair(lineBegin);
+    printPair(lineEnd);
 }
